70723c27267c4f23f9aa558dc336a8b6a7e7203a
M-Asch <Aschme_m1@denison.edu>
2021-11-15 13:21:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,13 @@ import os
 def dictBuilder():
     alignments = {}
     currentDirect = os.getcwd()
-    file = open(currentDirect+'/England (DEC052020)/HCOV19-ENGLAND-051220-A.fasta', r)
-    print(file)
+    fileNames = ['\England (DEC052020)\HCOV19-ENGLAND-051220-A.fasta', '\England (DEC082020)\HCOV19-ENGLAND-081220-A.fasta', '\England (NOV032020)\HCOV19-ENGLAND-031220-A.fasta']
+    file = open(currentDirect+, "r")
+
 
 
 def main():
+    dictBuilder()
     return 0
+
+main()
