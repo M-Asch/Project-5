6addfae833ed23af825c4f3fd5a105538cde6439
M-Asch <Aschme_m1@denison.edu>
2021-11-15 13:17:34 -0500

    stuff

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+#================================
+# Mitchell Aschmeyer and Riley Collins
+#
+#
+#================================
+import os
 
+
+def dictBuilder():
+    alignments = {}
+    currentDirect = os.getcwd()
+    file = open(currentDirect+'/England (DEC052020)/HCOV19-ENGLAND-051220-A.fasta', r)
+    print(file)
+
+
+def main():
+    return 0
