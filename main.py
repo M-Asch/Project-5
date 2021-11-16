1d5f850f7e37f56c5b1ae69b56171c6a0229a223
M-Asch <Aschme_m1@denison.edu>
2021-11-16 15:31:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ import os
 def dictBuilder():
     alignments = {}
     currentDirect = os.getcwd()
-    fileNames = ['\England (DEC052020)\HCOV19-ENGLAND-051220-A.fasta', '\England (DEC082020)\HCOV19-ENGLAND-081220-A.fasta', '\England (NOV032020)\HCOV19-ENGLAND-031220-A.fasta']
-    file = open(currentDirect+, "r")
-
+    fileNames = ['\England (DEC052020)\HCOV19-ENGLAND-051220-A.fasta', '\England (DEC082020)\HCOV19-ENGLAND-081220-A.fasta', '\England (NOV032020)\HCOV19-ENGLAND-031120-A.fasta', '\England (NOV102020)\HCOV19-ENGLAND-101120-A.fasta', '\England (NOV272020)\HCOV19-ENGLAND-271120-A.fasta']
+    for name in fileNames:
+        file = open(currentDirect + name, "r")
+        file = file.readlines()
+        alignments[file[0]] = file[1:]
+    return(alignments)
 
 
 def main():
